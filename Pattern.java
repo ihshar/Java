cf334ccfa910d04fdfc2cc4e7050bee37cc81c05
Himanshu Sharma <himanshu@HimanshusLaptop.bbrouter>
2024-06-21 02:22:36 +0530

    one more practice question added

--- Pattern.java
+++ Pattern.java
@@ -8,6 +8,7 @@ public class Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.close();
         // int m = sc.nextInt();
 
         // ****
@@ -152,18 +153,18 @@ public class Pattern {
         // 1 0 1 0 1  
 
 
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=i;j++){
-        //         int sum = i+j;
-        //         if(sum % 2 == 0){
-        //             System.out.print("1 ");
-        //         }
-        //         else{
-        //             System.out.print("0 ");
-        //         }
-        //     }
-        //     System.out.println(" ");
-        // }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                int sum = i+j;
+                if(sum % 2 == 0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println(" ");
+        }
 
 
         
